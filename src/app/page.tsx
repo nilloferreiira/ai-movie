b0db0b642bd5025e3592b8dcc4b729b00cf61d7c
nilloferreiira <danillodana@outlook.com>
2023-12-21 18:30:56 -0300

    responsividade notebook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       flex flex-col 
       items-center
       mx-auto
-      mt-4
-      lg:mt-32 md:mt-32 
+      my-4
+      lg:mt-32 md:mt-32 md:my-24
       lg:py-16 lg:px-20
       py-8 px-6      
       border-4 border-principal-pink rounded-lg
